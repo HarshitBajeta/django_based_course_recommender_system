2d695e160f7aa0c1e44fd1b8a189b46afec6eff9
HarshitBajeta <harshitbajeta22@gmail.com>
2023-06-11 05:47:38 +0530

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import pickle
+import numpy as np
+
+def initialize_app():
+    popular_df = pickle.load(open('myapp/data/courses.pkl', 'rb'))
+    similarity_scores = pickle.load(open('myapp/data/similarity.pkl', 'rb'))
+    return popular_df, similarity_scores
+
+popular_df, similarity_scores = initialize_app()
+
+def index(request):
+    return render(request, 'index1.html', {
+        'course_name': list(popular_df['course_name'].values),
+        'course_univ': list(popular_df['University'].values),
+        'course_difficulty': list(popular_df['difficulty_level'].values),
+        'course_rating': list(popular_df['course_rating'].values),
+    })
+
+def recommend_ui(request):
+    return render(request, 'recommend1.html')
+
+def recommend(request):
+    if request.method == 'GET':
+        user_input = request.GET.get('user_input')
+        try:
+            index = popular_df[popular_df['course_name'] == user_input].index[0]
+        except:
+            return render(request, 'error.html', {'message': 'Course not found'})
+
+        distances = sorted(list(enumerate(similarity_scores[index])), reverse=True, key=lambda x: x[1])
+        data = []
+        for i in distances[1:6]:
+            item = []
+            item.append(popular_df.iloc[i[0]].course_name)
+            item.append(popular_df.iloc[i[0]].University)
+            item.append(popular_df.iloc[i[0]].difficulty_level)
+            item.append(popular_df.iloc[i[0]].course_rating)
+            data.append(item)
+        print(data)
+        return render(request, 'recommend1.html', {'data': data})
+    return HttpResponse('Invalid request')
